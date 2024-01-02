5a4912983f621df5d54d3e4a00d586527e094af6
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2024-01-02 15:36:55 +0400

    rmmod

--- module/kvm_ivshmem.c
+++ module/kvm_ivshmem.c
@@ -208,11 +208,11 @@ static long kvm_ivshmem_ioctl(struct file *filp, unsigned int cmd,
 
   case SHMEM_IOCSETPEERID:
     spin_lock(&rawhide_irq_lock);
-    if (copy_from_user(&tmp, (void __user *)arg, sizeof(tmp))) {
-      printk(KERN_ERR "KVM_IVSHMEM: SHMEM_IOCSETPEERID: invalid arument");
-      return -EINVAL;
-    }
-    filp->private_data = (void*) (unsigned long) tmp;
+    // if (copy_from_user(&tmp, (void __user *)arg, sizeof(tmp))) {
+    //   printk(KERN_ERR "KVM_IVSHMEM: SHMEM_IOCSETPEERID: invalid arument");
+    //   return -EINVAL;
+    // }
+    // filp->private_data = (void*) (unsigned long) tmp;
     // TODO: remove
     printk(KERN_ERR "KVM_IVSHMEM: SHMEM_IOCSETPEERID: set peer id 0x%x",
             /*filp->private_data*/tmp);
