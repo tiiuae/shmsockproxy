fc50cd425d2962d5a85f22ca73901ac3aeb25c1c
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2024-01-03 09:37:03 +0400

    rmmod

--- app/server.bash
+++ app/server.bash
@@ -4,6 +4,13 @@ SOCKET=./server.sock
 DEVICE=/dev/ivshmem
 MODDIR=~ghaf/shmsockproxy/module
 
+pid=`ps | grep memsocket | grep -v grep`
+result=$?
+if [ result == 0 ]
+then
+  kill $(echo $pid | awk '{print $1}')
+fi
+
 if test -e "$SOCKET"; then
   echo "Removing $SOCKET"
   rm "$SOCKET"
