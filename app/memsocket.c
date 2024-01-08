45b908e866860292c2e759bb5fd14274f23e1ddb
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2024-01-08 15:44:41 +0400

    preparing to threads

--- app/memsocket.c
+++ app/memsocket.c
@@ -43,7 +43,7 @@
   {                                                                            \
     char tmp1[256], tmp2[256];                                                 \
     sprintf(tmp2, fmt, __VA_ARGS__);                                           \
-    sprintf(tmp1, "%s:%d: %s\n", __FUNCTION__, __LINE__, tmp2);                \
+    sprintf(tmp1, "%d %s:%d: %s\n", instance_no, __FUNCTION__, __LINE__, tmp2);                \
     errno = 0;                                                                 \
     report(tmp1, 0);                                                           \
   }
