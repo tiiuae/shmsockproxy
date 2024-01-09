00b24b14a0eea1362f67a02658fb5961fe421091
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2024-01-09 11:04:55 +0400

    preparing to threads

--- app/client.bash
+++ app/client.bash
@@ -18,6 +18,6 @@ echo "Loading shared memory module"
 sudo rmmod kvm_ivshmem ; sudo insmod $MODDIR/kvm_ivshmem.ko; sudo chmod a+rwx /dev/ivshmem
 fi
 
-./memsocket -c "$SOCKET" 2 &
+./memsocket -c "$SOCKET" &
 echo "Starting waypipe"
 waypipe  -s "$SOCKET" client
