ef2b4754d9273f221ce497417a25c50ab66cfeac
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2024-01-10 13:57:09 +0400

    Test bug fix: re-added threads

--- app/memsocket.c
+++ app/memsocket.c
@@ -661,23 +661,27 @@ int main(int argc, char **argv) {
   }
 
   /* On client site start thread for each display VM */
-  if (!run_as_server)
-    thread_count = 1; //VM_COUNT;
+  if (!run_as_server) {
+    thread_count = VM_COUNT;
 
   for (i = 0; i < thread_count; i++) {
-    thread_init(i);
-    res = pthread_create(&threads[i], NULL, run, (void*)(intptr_t) i);
-    if (res) {
-      ERROR("Thread id=%d", i);
-      FATAL("Cannot create a thread");
+      thread_init(i);
+      res = pthread_create(&threads[i], NULL, run, (void*)(intptr_t) i);
+      if (res) {
+        ERROR("Thread id=%d", i);
+        FATAL("Cannot create a thread");
+      }
     }
-  }
 
-  for (i = 0; i < thread_count; i++) {
-    res = pthread_join(threads[i], NULL);
-    if (res) {
-      ERROR("error %d waiting for the thread #%d", res, i);
+    for (i = 0; i < thread_count; i++) {
+      res = pthread_join(threads[i], NULL);
+      if (res) {
+        ERROR("error %d waiting for the thread #%d", res, i);
+      }
     }
+  } else { /* server mode - only one instance */
+    thread_init(instance_no);
+    run((void*) instance_no);
   }
 
   return 0;
