e56d5c86df8fbccdfa36f6c1b0fd227c47e0c9cf
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2024-03-12 15:39:45 +0400

    module: removed smb

--- drivers/char/ivshmem/kvm_ivshmem.c
+++ drivers/char/ivshmem/kvm_ivshmem.c
@@ -428,7 +428,6 @@ static irqreturn_t kvm_ivshmem_interrupt(int irq, void *dev_instance) {
       }
       spin_lock(&rawhide_irq_lock);
       peer_resource_count[i] = 1;
-      // smp_mb();
       spin_unlock(&rawhide_irq_lock);
       wake_up_interruptible(&peer_data_ready_wait_queue[i]);
       return IRQ_HANDLED;
@@ -443,7 +442,6 @@ static irqreturn_t kvm_ivshmem_interrupt(int irq, void *dev_instance) {
       }
       spin_lock(&rawhide_irq_lock);
       local_resource_count[i] = 1;
-      // smp_mb();
       spin_unlock(&rawhide_irq_lock);
       wake_up_interruptible(&local_data_ready_wait_queue[i]);
       return IRQ_HANDLED;
